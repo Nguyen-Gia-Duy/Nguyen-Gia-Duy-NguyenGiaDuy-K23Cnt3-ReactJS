2eb690cd8311835f19b1aca9b62dc1317a907146
unknown <nguyengiaduy2005@gmail.com>
2025-03-12 13:54:31 +0700

    update

--- ngd-2310900031-k23cnt3-exam/ngd-2310900031-k23cnt3-exam/src/NgdApp.js
+++ ngd-2310900031-k23cnt3-exam/ngd-2310900031-k23cnt3-exam/src/NgdApp.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import NgdProductList from "./components/NgdProductList";
+import NgdProductAdd from "./components/NgdProductAdd";
 
-function App() {
+
+
+const NgdApp = () => {
+  //khởi tạo thành viên
+  const [members, setMembers] = useState([
+    { id: "230001", fullname: "Nguyễn Gia Duy", username: "Nguyen Duy", password: "123456" },
+    { id: "230002", fullname: "Trần Thị A", username: "A", password: "abcdef" },
+    { id: "230003", fullname: "Lê Văn B", username: "B", password: "qwerty" },
+  ]);
+//thêm mới
+  const addMember = (member) => {
+    setMembers([...members, member]);
+  };
+    // Hàm xóa thành viên theo ID
+    const removeMember = (id) => {
+      setMembers(members.filter(member => member.id !== id));
+  };
+
+  // Hàm cập nhật thông tin thành viên
+  const updateMember = (updatedMember) => {
+    setMembers(members.map(member => (member.id === updatedMember.id ? updatedMember : member)));
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <h1 className="text-primary">Danh sách thành viên</h1>
+      <NgdProductList members={members} removeMember={removeMember} updateMember={updateMember}/>
+      <NgdProductAdd addMember={addMember}/>
+      
     </div>
   );
-}
+};
 
-export default App;
+export default NgdApp;
